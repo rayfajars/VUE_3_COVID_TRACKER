a97675e776eaf38764f896f807f75f681b1a3db0
Ray Fajar S <rayfajars.com>
2022-09-09 07:04:14 +0700

    first commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,28 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    Hello Home
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+// import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    // HelloWorld
+  },methods:{
+    async fetchCovidData(){
+      const res = await fetch('https://api.covid19api.com/summary')
+      const data = await res.json()
+      return data;
+    }
+  },  
+  async created(){
+    const data = await this.fetchCovidData()
+
+    console.log(data)
   }
 }
 </script>
